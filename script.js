83175af7a5d71d9a75b52b9b092f5ddc7cf20334
nicolashaldemann <114082663+nicolashaldemann@users.noreply.github.com>
2022-10-26 13:19:12 +0200

    Slideshow

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     });
   });
 });
+
+var simpleSlides = function () {
+  var i;
+  var slides = document.querySelectorAll(".slides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].setAttribute("style", "display:none");
+  }
+  count++;
+  if (count > slides.length) {
+    count = 1;
+  }
+  slides[count - 1].setAttribute("style", "display:block");
+  setTimeout(simpleSlides, 3600);
+};
+
+var count = 0;
+simpleSlides();

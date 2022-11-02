3406d30805ae6db850ca8a0ea68199e17e4a4804
nicolashaldemann <114082663+nicolashaldemann@users.noreply.github.com>
2022-11-02 15:08:29 +0100

    Timeline

--- script.js
+++ script.js
@@ -26,56 +26,31 @@ var simpleSlides = function () {
 var count = 0;
 simpleSlides();
 
-("use strict");
+(function () {
+  const items = document.querySelectorAll(".timeline-section li");
 
-function qs(selector, all = false) {
-  return all
-    ? document.querySelectorAll(selector)
-    : document.querySelector(selector);
-}
-
-const sections = qs(".section", true);
-const timeline = qs(".timeline");
-const line = qs(".line");
-line.style.bottom = `calc(100% - 20px)`;
-let prevScrollY = window.scrollY;
-let up, down;
-let full = false;
-let set = 0;
-const targetY = window.innerHeight * 0.8;
-
-function scrollHandler(e) {
-  const { scrollY } = window;
-  up = scrollY < prevScrollY;
-  down = !up;
-  const timelineRect = timeline.getBoundingClientRect();
-  const lineRect = line.getBoundingClientRect(); // const lineHeight = lineRect.bottom - lineRect.top;
-
-  const dist = targetY - timelineRect.top;
-  console.log(dist);
-
-  if (down && !full) {
-    set = Math.max(set, dist);
-    line.style.bottom = `calc(100% - ${set}px)`;
+  function isElementInViewport(el) {
+    let rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
   }
 
-  if (dist > timeline.offsetHeight + 50 && !full) {
-    full = true;
-    line.style.bottom = `-50px`;
-  }
-
-  sections.forEach((item) => {
-    // console.log(item);
-    const rect = item.getBoundingClientRect(); //     console.log(rect);
-
-    if (rect.top + item.offsetHeight / 5 < targetY) {
-      item.classList.add("show-me");
+  function slideIn() {
+    for (let i = 0; i < items.length; i++) {
+      if (isElementInViewport(items[i])) {
+        items[i].classList.add("slide-in");
+      } else {
+        items[i].classList.remove("slide-in");
+      }
     }
-  }); // console.log(up, down);
+  }
 
-  prevScrollY = window.scrollY;
-}
-
-scrollHandler();
-line.style.display = "block";
-window.addEventListener("scroll", scrollHandler);
+  window.addEventListener("load", slideIn);
+  window.addEventListener("scroll", slideIn);
+  window.addEventListener("resize", slideIn);
+})();

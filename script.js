bff79325e1d82534cc9e937b866ed83f559d5eab
nicolashaldemann <114082663+nicolashaldemann@users.noreply.github.com>
2022-11-02 18:40:55 +0100

    new branch

--- script.js
+++ script.js
@@ -57,3 +57,19 @@ simpleSlides();
   window.addEventListener("scroll", slideIn);
   window.addEventListener("resize", slideIn);
 })();
+
+/*mobile button*/
+const navbarToggle = navbar.querySelector("#navbar-toggle");
+const navbarMenu = document.querySelector("#navbar-menu");
+const navbarLinksContainer = navbarMenu.querySelector(".navbar-links");
+let isNavbarExpanded = navbarToggle.getAttribute("aria-expanded") === "true";
+
+const toggleNavbarVisibility = () => {
+  isNavbarExpanded = !isNavbarExpanded;
+  navbarToggle.setAttribute("aria-expanded", isNavbarExpanded);
+};
+
+navbarToggle.addEventListener("click", toggleNavbarVisibility);
+
+navbarLinksContainer.addEventListener("click", (e) => e.stopPropagation());
+navbarMenu.addEventListener("click", toggleNavbarVisibility);
